91d3d494fe37583ca4c893fa9ec772679916373e
EvandriRidho <152156672+EvandriRidho@users.noreply.github.com>
2023-11-29 17:54:30 +0700

    Add files via upload

---
+++ 2.Perulangan.js
@@ -0,0 +1,26 @@
+// While Loops
+let j = 1;
+while( j <= 5) {
+    console.log("Hello Saya Davina " + j +"x");
+    j++;
+}
+
+// For Loops
+for(i = 1; i <=5; i++) {
+    console.log("Hello Saya Evandri " +i+ "x");
+}
+
+// Latihan Perulangan
+let noAngkot = 1;
+let angkoBeroperasi = 6;
+let angkotTidakBeroperasi = 7;
+let jmlAngkot = 10;
+while(noAngkot <= angkoBeroperasi) {
+    console.log("Angkot No. "+ noAngkot + " Beroperasi dengan baik.");
+    noAngkot++;
+}
+for(angkotTidakBeroperasi; angkotTidakBeroperasi <= 10; angkotTidakBeroperasi++) {
+    console.log("Angkot No. "+ angkotTidakBeroperasi + " Sedang tidak beroperasi");
+}
+
+
